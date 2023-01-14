bee37f189fda4a74dc861a30197c74e029b63e53
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-01-14 21:42:38 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod pokeapi;
 
 use clap::Parser;
 use pokeapi::get::get_pokemon;
+use rand::Rng;
 
 #[derive(Parser, Debug)]
 #[command(version)]
@@ -17,11 +18,25 @@ struct Args {
 async fn main() -> Result<(), reqwest::Error> {
     let args = Args::parse();
 
+    if args.random {
+        let mut rng = rand::thread_rng();
+        let id = rng.gen_range(1..1010);
+
+        match get_pokemon(id.to_string()).await {
+            Ok(res) => println!("{:#?}", res),
+            Err(err) => eprintln!("\x1b[1;31m[ERROR]\x1b[0m An error occurred \n{}", err),
+        }
+
+        return Ok(());
+    }
+
     if args.id != None {
         match get_pokemon(args.id.unwrap()).await {
             Ok(res) => println!("{:#?}", res),
             Err(err) => eprintln!("\x1b[1;31m[ERROR]\x1b[0m An error occurred \n{}", err),
         }
+
+        return Ok(());
     }
 
     Ok(())
